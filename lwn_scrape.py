c45335b64656ab4bc24a86dc566ab711554d5bb8
Jesper Kristiansson <jesper.g.kristiansson@gmail.com>
2026-01-28 11:27:07 +0100

    Add a separator for subscription-only articles

--- lwn_scrape.py
+++ lwn_scrape.py
@@ -4,6 +4,8 @@ import time
 import argparse
 import html
 
+from datetime import datetime, timedelta
+
 #@profile
 def main():
     parser = argparse.ArgumentParser(description='Scrapes all articles from lwn.net and presents them in chronological order')
@@ -19,8 +21,28 @@ def main():
     articles = set(re.findall(article_multiline_regex, page.text))
 
     sorted_articles = sorted(articles, key=lambda t: int(t[0][10:]),reverse=args.reverse)
-    article_string_generator = (f'{title}\nhttps://lwn.net{link}\n{date}' for link, title, date in sorted_articles)
-    out = '\n\n'.join(article_string_generator)
+
+    subscriptionCutoffDatetime = datetime.now() - timedelta(weeks=2)
+    cutoffIndex = 0
+    for i, e in reversed(list(enumerate(sorted_articles))):
+        dt = datetime.strptime(e[2], "%B %d, %Y")
+        if dt < subscriptionCutoffDatetime:
+            cutoffIndex = i + 1
+            break
+
+    preCutoffArticles = sorted_articles[:cutoffIndex]
+    postCutoffArticles = sorted_articles[cutoffIndex:]
+
+    def articleToString(article):
+        link, title, date = article
+        return f'{title}\nhttps://lwn.net{link}\n{date}'
+
+    delimiter = '\n\n'
+
+    preCutoffArticlesString = delimiter.join(articleToString(art) for art in preCutoffArticles)
+    postCutoffArticlesString = delimiter.join(articleToString(art) for art in postCutoffArticles)
+    estimatedCutoffString = '----------------- Estimated subscription cutoff -----------------'
+    out = delimiter.join((preCutoffArticlesString, estimatedCutoffString, postCutoffArticlesString))
     out = html.unescape(out)
     print(out)
 main()
